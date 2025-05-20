b28cccfd72031176a83e45899549bff9cb8944fe
AnugrahaLK <anugrahalkacharya347@gmail.com>
2025-05-21 00:50:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 // Connect to MetaMask
 let account;
-const contractAddress = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
+const contractAddress = "0x**********";
 const contractABI = [ 
   {
     "inputs": [
